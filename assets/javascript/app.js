4786848077f68c8f0033a7fee6291739e3736207
Chris R <chris.rocha@>
2017-05-23 21:15:15 -0500

    adding skeleton of the app object

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+var gotPlans = {
+    lookupUser: function(){
+        //whatever
+    },
+    coinFlip: function(){
+
+    },
+    queryCity: function(){
+
+    },
+    filterCuisine: function(){
+
+    },
+    filterPrice: function(){
+
+    },
+    filterRating: function(){
+
+    },
+    displayResultZomato: function(){
+
+    },
+    randomRestaurant: function(){
+        //For feeling adventurous under zamato api
+    },
+    displayResultSpoon: function(){
+
+    },
+    searchByIngredients: function(){
+
+    }
+}
